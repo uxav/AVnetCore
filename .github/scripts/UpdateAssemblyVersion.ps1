2c7f74be66e4bb2712068bdeafa13161a5abb2d9
Mike Jobson <mike@ux.digital>
2020-08-04 16:21:08 +0100

    Workflow changes

--- .github/scripts/UpdateAssemblyVersion.ps1
+++ .github/scripts/UpdateAssemblyVersion.ps1
@@ -2,26 +2,26 @@ function Update-SourceVersion {
     Param ([string]$Version)
     #$fullVersion = $Version
     $baseVersion = [regex]::Match($Version, "(\d+.\d+.\d+).*").captures.groups[1].value
-    $NewAssemblyVersion = ‘AssemblyVersion("‘ + $baseVersion + ‘.*")’
+    $NewAssemblyVersion = 'AssemblyVersion("' + $baseVersion + '.*")'
     Write-Output "AssemblyVersion = $NewAssemblyVersion"
-    $NewAssemblyInformationalVersion = ‘AssemblyInformationalVersion("‘ + $Version + ‘")’
+    $NewAssemblyInformationalVersion = 'AssemblyInformationalVersion("' + $Version + '")'
     Write-Output "AssemblyInformationalVersion = $NewAssemblyInformationalVersion"
 
     foreach ($o in $input) {
         Write-output $o.FullName
-        $TmpFile = $o.FullName + “.tmp”
+        $TmpFile = $o.FullName + ".tmp"
         get-content $o.FullName |
                 ForEach-Object {
-                    $_ -replace ‘AssemblyVersion\(".*"\)’, $NewAssemblyVersion } |
+                    $_ -replace 'AssemblyVersion\(".*"\)', $NewAssemblyVersion } |
                 ForEach-Object {
-                    $_ -replace ‘AssemblyInformationalVersion\(".*"\)’, $NewAssemblyInformationalVersion
+                    $_ -replace 'AssemblyInformationalVersion\(".*"\)', $NewAssemblyInformationalVersion
                 }  > $TmpFile
         move-item $TmpFile $o.FullName -force
     }
 }
 
 function Update-AllAssemblyInfoFiles ( $version ) {
-    foreach ($file in “AssemblyInfo.cs”, “AssemblyInfo.vb” ) {
+    foreach ($file in "AssemblyInfo.cs", "AssemblyInfo.vb" ) {
         get-childitem -Path $Env:GITHUB_WORKSPACE -recurse | Where-Object { $_.Name -eq $file } | Update-SourceVersion $version ;
     }
 }
@@ -33,8 +33,8 @@ if ($r.Success) {
     Update-AllAssemblyInfoFiles $args[0];
 }
 else {
-    Write-Output ” “;
-    Write-Output “Error: Input version does not match x.y.z format!”
-    Write-Output ” “;
+    Write-Output " ";
+    Write-Output "Error: Input version does not match x.y.z format!"
+    Write-Output " ";
     Write-Output "Unable to apply version to AssemblyInfo.cs files";
 }
