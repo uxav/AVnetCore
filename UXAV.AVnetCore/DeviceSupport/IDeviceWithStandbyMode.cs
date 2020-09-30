d51184316ee0eb49f7eaec861713fe728edd0c86
Mike Jobson <mike@ux.digital>
2020-09-30 11:42:57 +0100

    Added device standby mode

---
+++ UXAV.AVnetCore/DeviceSupport/IDeviceWithStandbyMode.cs
@@ -0,0 +1,12 @@
+namespace UXAV.AVnetCore.DeviceSupport
+{
+    public interface IDeviceWithStandbyMode : IDevice
+    {
+        bool InStandby { get; }
+        void Wake();
+        void Sleep();
+        event StandbyModeChangedEventHandler InStandbyChanged;
+    }
+
+    public delegate void StandbyModeChangedEventHandler(IDeviceWithStandbyMode device, bool inStandby);
+}
