5e242ce6c2c348f06a34fa63cf9ac3f1859b2d87
Mike Jobson <mike@ux.digital>
2020-10-12 06:26:01 +0100

    Added ip id tool for assignment

---
+++ UXAV.AVnetCore/DeviceSupport/IpIdHelper.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UXAV.AVnetCore.DeviceSupport
+{
+    public class IpIdHelper
+    {
+        private readonly ConcurrentBag<uint> _usedValues;
+        private const uint StartId = 0x03;
+        private const uint MaxId = 0xFE;
+        public IpIdHelper()
+        {
+            _usedValues = new ConcurrentBag<uint>();
+        }
+
+        public IpIdHelper(IEnumerable<uint> usedIpIds)
+        {
+            _usedValues = new ConcurrentBag<uint>(usedIpIds);
+        }
+
+        public uint GetNextValue()
+        {
+            return GetNextValueStartingAt(StartId);
+        }
+
+        public uint GetNextValueStartingAt(uint ipId)
+        {
+            if(ipId < 0x03) throw new IndexOutOfRangeException("id must be greater than 0x03");
+            for (var id = ipId; id <= MaxId; id++)
+            {
+                if(_usedValues.Contains(id)) continue;
+                return id;
+            }
+            throw new InvalidOperationException("No more ID's available");
+        }
+    }
+}
