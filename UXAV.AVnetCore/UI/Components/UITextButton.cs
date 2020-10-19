0f51f2e063c366936027c5216e3947a9e6d3251c
Mike Jobson <mike@ux.digital>
2020-10-19 21:26:37 +0100

    Added id in ctor for buttons

--- UXAV.AVnetCore/UI/Components/UITextButton.cs
+++ UXAV.AVnetCore/UI/Components/UITextButton.cs
@@ -4,29 +4,23 @@ namespace UXAV.AVnetCore.UI.Components
 {
     public class UITextButton : UIButton, ITextItem
     {
-        public UITextButton(ISigProvider sigProvider, uint digitalJoinNumber, uint serialJoinNumber)
-            : base(sigProvider, digitalJoinNumber)
-        {
-            SerialJoinNumber = serialJoinNumber;
-        }
-
-        public UITextButton(ISigProvider sigProvider, uint digitalJoinNumber, uint enableJoinNumber,
-            uint visibleJoinNumber, uint serialJoinNumber)
-            : base(sigProvider, digitalJoinNumber, enableJoinNumber, visibleJoinNumber)
+        public UITextButton(ISigProvider sigProvider, uint digitalJoinNumber, uint serialJoinNumber,
+            uint enableJoinNumber = 0, uint visibleJoinNumber = 0, uint id = 0)
+            : base(sigProvider, digitalJoinNumber, enableJoinNumber, visibleJoinNumber, id)
         {
             SerialJoinNumber = serialJoinNumber;
         }
 
         public UITextButton(ISigProvider sigProvider, string pressJoinName, string feedbackJoinName,
-            string serialJoinName)
-            : base(sigProvider, pressJoinName, feedbackJoinName)
+            string serialJoinName, uint id = 0)
+            : base(sigProvider, pressJoinName, feedbackJoinName, id)
         {
             SerialJoinNumber = SigProvider.StringInput[serialJoinName].Number;
         }
 
         public UITextButton(ISigProvider sigProvider, string pressJoinName,
-            string feedbackJoinName, string enableJoinName, string visibleJoinName, string serialJoinName)
-            : base(sigProvider, pressJoinName, feedbackJoinName, enableJoinName, visibleJoinName)
+            string feedbackJoinName, string enableJoinName, string visibleJoinName, string serialJoinName, uint id = 0)
+            : base(sigProvider, pressJoinName, feedbackJoinName, enableJoinName, visibleJoinName, id)
         {
             SerialJoinNumber = SigProvider.StringInput[serialJoinName].Number;
         }
