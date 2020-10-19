ebfaf89707595e32fb4d4aa0b7d4742d56e7d06c
Mike Jobson <mike@ux.digital>
2020-10-19 16:13:03 +0100

    Added UI Keypad

---
+++ UXAV.AVnetCore/UI/Components/UIKeypad.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Crestron.SimplSharpPro;
+using UXAV.AVnetCore.DeviceSupport;
+
+namespace UXAV.AVnetCore.UI.Components
+{
+    public class UIKeypad : UIObject, IButtons
+    {
+        private readonly UIButtonCollection _buttons = new UIButtonCollection();
+
+        public UIKeypad(ISigProvider sigProvider, uint startJoin)
+            : base(sigProvider)
+        {
+            var enumValues = Enum.GetValues(typeof(UIKeypadButtonValue)).Cast<UIKeypadButtonValue>();
+            foreach (var value in enumValues)
+            {
+                var btn = new UIButton(this, startJoin + (uint) value);
+                btn.SetId((uint) value);
+                _buttons.Add(btn);
+            }
+        }
+
+        public UIKeypad(SmartObject smartObject)
+            : base(smartObject)
+        {
+            var enumValues = Enum.GetValues(typeof(UIKeypadButtonValue)).Cast<UIKeypadButtonValue>();
+            foreach (var value in enumValues)
+            {
+                UIButton btn;
+                if (value == UIKeypadButtonValue.Key0)
+                {
+                    btn = new UIButton(this, 10);
+                    btn.SetId(0);
+                    _buttons.Add(btn);
+                    continue;
+                }
+
+                if (value >= UIKeypadButtonValue.Misc1)
+                {
+                    btn = new UIButton(this, 1 + (uint) value);
+                    btn.SetId((uint) value);
+                    _buttons.Add(btn);
+                    continue;
+                }
+
+                btn = new UIButton(this, (uint) value);
+                btn.SetId((uint) value);
+                _buttons.Add(btn);
+            }
+        }
+
+        public IEnumerable<IButton> Buttons => _buttons;
+    }
+
+    public enum UIKeypadButtonValue
+    {
+        Key0,
+        Key1,
+        Key2,
+        Key3,
+        Key4,
+        Key5,
+        Key6,
+        Key7,
+        Key8,
+        Key9,
+        Misc1,
+        Misc2
+    }
+}
