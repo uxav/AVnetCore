a7be418a54c4baa15eff7279457b5589cc917e29
Mike Jobson <mike@ux.digital>
2020-11-02 16:01:15 +0000

    Added name property to ISourceTarget.cs

--- UXAV.AVnetCore/Models/ISourceTarget.cs
+++ UXAV.AVnetCore/Models/ISourceTarget.cs
@@ -5,6 +5,7 @@ namespace UXAV.AVnetCore.Models
 {
     public interface ISourceTarget
     {
+        string Name { get; }
         SourceBase GetCurrentSource(uint forIndex = 1);
         Task<bool> SelectSourceAsync(SourceBase source, uint forIndex = 1);
     }
