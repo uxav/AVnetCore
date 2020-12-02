d6e9ffa2059471206d4ddb222f7b706393133680
Mike Jobson <mike@ux.digital>
2020-12-02 13:20:25 +0000

    Added options for change increments on UIGenericSubPageReferenceList

--- UXAV.AVnetCore/UI/Components/UIGenericSubPageReferenceList.cs
+++ UXAV.AVnetCore/UI/Components/UIGenericSubPageReferenceList.cs
@@ -4,8 +4,9 @@ namespace UXAV.AVnetCore.UI.Components
 {
     public class UIGenericSubPageReferenceList : UISubPageReferenceList
     {
-        public UIGenericSubPageReferenceList(SmartObject smartObject)
-            : base(smartObject, 1, 1, 1,
+        public UIGenericSubPageReferenceList(SmartObject smartObject, uint digitalJoinIncrement = 1,
+            uint analogJoinIncrement = 1, uint serialJoinIncrement = 1)
+            : base(smartObject, digitalJoinIncrement, analogJoinIncrement, serialJoinIncrement,
                 (list, id) => new UIGenericSubPageReferenceListItem(list, id))
         {
             foreach (var item in this)
