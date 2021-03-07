f478693ad320fc7f62025a9fbe23e1516d8c2323
Mike Jobson <mike@ux.digital>
2021-03-07 08:40:09 +0000

    Added in progload command for web app

--- UXAV.AVnetCore/WebScripting/InternalApi/AppControlApiHandler.cs
+++ UXAV.AVnetCore/WebScripting/InternalApi/AppControlApiHandler.cs
@@ -1,5 +1,8 @@
+using System.IO;
+using System.Threading.Tasks;
 using Crestron.SimplSharp;
 using Newtonsoft.Json.Linq;
+using UXAV.AVnetCore.Models;
 using UXAV.Logging;
 
 namespace UXAV.AVnetCore.WebScripting.InternalApi
@@ -29,6 +32,32 @@ namespace UXAV.AVnetCore.WebScripting.InternalApi
                     Logger.Warn("Remote reboot requested from {0}", Request.UserHostAddress);
                     WriteResponse("App will now send reboot command!");
                     System.RebootAppliance();
+                    return;
+                case "load":
+                    Logger.Warn("Remote load requested from {0}", Request.UserHostAddress);
+                    var appDir = new DirectoryInfo(SystemBase.ProgramApplicationDirectory);
+                    var files = appDir.GetFiles("*.cpz");
+                    if (files.Length == 1)
+                    {
+                        Logger.Warn($"Will send progload command for app {InitialParametersClass.ApplicationNumber}," +
+                                    $"file found: {files[0].FullName}");
+                        WriteResponse($"App will load \"{files[0].FullName}\" now!");
+                        Task.Run(() =>
+                        {
+                            CrestronConsole.SendControlSystemCommand(
+                                $"progload -p:{InitialParametersClass.ApplicationNumber}", ref response);
+                            Logger.Highlight($"progload response: \"{response}\"");
+                        });
+                        return;
+                    }
+
+                    if (files.Length == 0)
+                    {
+                        HandleError(404, "Not Found", "CPZ file could not be found");
+                        return;
+                    }
+
+                    HandleError(409, "Conflict", "More than one CPZ file found in app directory");
                     return;
                 default:
                     HandleError(400, "Bad Request", $"Unknown command: \"{cmd}\"");
