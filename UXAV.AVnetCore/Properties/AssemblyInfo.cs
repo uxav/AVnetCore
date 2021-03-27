d5dcaf319d85b2b178be08ab06956ea80826b958
Mike Jobson <mike@ux.digital>
2021-03-27 10:16:22 +0000

    Updated assembly info which will now specify base version

--- UXAV.AVnetCore/Properties/AssemblyInfo.cs
+++ UXAV.AVnetCore/Properties/AssemblyInfo.cs
@@ -19,14 +19,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9B3BAD80-9F92-4687-915E-C02F3910DCEF")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
