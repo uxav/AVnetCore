bb26384dd347ec862b5a56d9159e8af0f8206e2c
Mike Jobson <mike@ux.digital>
2021-03-27 10:22:33 +0000

    Replaced github actions scripts

--- .github/scripts/UpdateAssemblyVersion.ps1
+++ .github/scripts/UpdateAssemblyVersion.ps1
@@ -1,40 +1,49 @@
-function Update-SourceVersion {
-    Param ([string]$Version)
-    #$fullVersion = $Version
-    $baseVersion = [regex]::Match($Version, "(\d+.\d+.\d+).*").captures.groups[1].value
-    $NewAssemblyVersion = 'AssemblyVersion("' + $baseVersion + '.*")'
-    Write-Output "AssemblyVersion = $NewAssemblyVersion"
-    $NewAssemblyInformationalVersion = 'AssemblyInformationalVersion("' + $Version + '")'
-    Write-Output "AssemblyInformationalVersion = $NewAssemblyInformationalVersion"
+param([string] $Path, [string] $Version)
 
-    foreach ($o in $input) {
-        Write-output $o.FullName
-        $TmpFile = $o.FullName + ".tmp"
-        get-content $o.FullName |
-                ForEach-Object {
-                    $_ -replace 'AssemblyVersion\(".*"\)', $NewAssemblyVersion } |
-                ForEach-Object {
-                    $_ -replace 'AssemblyInformationalVersion\(".*"\)', $NewAssemblyInformationalVersion
-                }  > $TmpFile
-        move-item $TmpFile $o.FullName -force
-    }
+$file = Get-Item $Path;
+Write-Output "File to open is $file"
+Write-Output "Specific version is $Version"
+if(!$Version)
+{
+    Write-Output("Version is not set, exiting!")
+    exit(1);
 }
 
-function Update-AllAssemblyInfoFiles ( $version ) {
-    foreach ($file in "AssemblyInfo.cs", "AssemblyInfo.vb" ) {
-        get-childitem -Path $Env:GITHUB_WORKSPACE -recurse | Where-Object { $_.Name -eq $file } | Update-SourceVersion $version ;
-    }
+$versionMatch = [Regex]::Match($Version, "^(\d+\.\d+\.\d+)(?:-([\w-]+))?$");
+
+if (!$versionMatch.Success) {
+    Write-Output("Version should be formatted x.x.x(-beta-1) etc");
+    exit(1);
 }
 
-# validate arguments
-$r = [System.Text.RegularExpressions.Regex]::Match($args[0], "\d+\.\d+\.\d+.*");
-if ($r.Success) {
-    Write-Output "Updating Assembly Version to $args ...";
-    Update-AllAssemblyInfoFiles $args[0];
+$baseVersion = $versionMatch.Captures.Groups[1].Value;
+if($versionMatch.Captures.Groups[2].Success)
+{
+    $preReleaseTag = $versionMatch.Captures.Groups[2].Value;
 }
-else {
-    Write-Output " ";
-    Write-Output "Error: Input version does not match x.y.z format!"
-    Write-Output " ";
-    Write-Output "Unable to apply version to AssemblyInfo.cs files";
+
+Write-Output "baseVersion = $baseVersion"
+$newAssemblyVersion = 'AssemblyVersion("' + $baseVersion + '.*")'
+Write-Output "AssemblyVersion = $NewAssemblyVersion"
+$newAssemblyFileVersion = 'AssemblyFileVersion("' + $baseVersion + '")'
+Write-Output "AssemblyFileVersion = $newAssemblyFileVersion"
+if($preReleaseTag)
+{
+    $newAssemblyInformationalVersion = 'AssemblyInformationalVersion("' + $baseVersion + '-' + $preReleaseTag + '")'
 }
+else
+{
+    $newAssemblyInformationalVersion = 'AssemblyInformationalVersion("' + $baseVersion + '")'
+}
+Write-Output "AssemblyInformationalVersion = $newAssemblyInformationalVersion"
+
+$TmpFile = $file.FullName + ".tmp"
+Get-Content $file.FullName |
+        ForEach-Object {
+            $_ -replace 'AssemblyVersion\(".*"\)', $newAssemblyVersion } |
+        ForEach-Object {
+            $_ -replace 'AssemblyFileVersion\(".*"\)', $newAssemblyFileVersion } |
+        ForEach-Object {
+            $_ -replace 'AssemblyInformationalVersion\(".*"\)', $newAssemblyInformationalVersion
+        }  > $TmpFile
+move-item $TmpFile $file.FullName -force
