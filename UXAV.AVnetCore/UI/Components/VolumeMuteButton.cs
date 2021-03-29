cb47acb9aa79b3de1760a8734f61b0271d5e9faf
Mike Jobson <mike@ux.digital>
2021-03-29 10:50:09 +0100

    Added VolumeMuteButton

---
+++ UXAV.AVnetCore/UI/Components/VolumeMuteButton.cs
@@ -0,0 +1,56 @@
+﻿using UXAV.AVnetCore.DeviceSupport;
+
+namespace UXAV.AVnetCore.UI.Components
+{
+    public class VolumeMuteButton : UIButton
+    {
+        private IVolumeControl _volumeControl;
+        private readonly bool _mutedState;
+
+        public VolumeMuteButton(ISigProvider sigProvider, uint digitalJoinNumber, uint enableJoinNumber = 0,
+            uint visibleJoinNumber = 0, uint id = 0, bool mutedState = true)
+            : base(sigProvider, digitalJoinNumber, enableJoinNumber, visibleJoinNumber, id)
+        {
+            _mutedState = mutedState;
+        }
+
+        public IVolumeControl VolumeControl
+        {
+            get => _volumeControl;
+            set
+            {
+                if (_volumeControl == value) return;
+                if (_volumeControl != null)
+                {
+                    _volumeControl.MuteChange -= VolumeControlOnMuteChange;
+                    ButtonEvent -= OnButtonEvent;
+                }
+
+                _volumeControl = value;
+
+                if (_volumeControl != null)
+                {
+                    _volumeControl.MuteChange += VolumeControlOnMuteChange;
+                    ButtonEvent += OnButtonEvent;
+                    SetFeedback(_volumeControl.Muted ? _mutedState : !_mutedState);
+                }
+            }
+        }
+
+        private void VolumeControlOnMuteChange(bool muted)
+        {
+            SetFeedback(muted ? _mutedState : !_mutedState);
+        }
+
+        private void OnButtonEvent(IButton button, ButtonEventArgs args)
+        {
+            if (args.EventType != ButtonEventType.Pressed) return;
+            if (_volumeControl != null)
+            {
+                var mute = !_volumeControl.Muted;
+                SetFeedback(mute ? _mutedState : !_mutedState);
+                _volumeControl.Muted = mute;
+            }
+        }
+    }
+}
