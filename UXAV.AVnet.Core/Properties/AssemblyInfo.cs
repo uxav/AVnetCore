f01862b921c345a9e6e698585832ca87d5e61e64
Mike Jobson <mike@ux.digital>
2021-06-15 17:33:13 +0100

    Tidy up

--- UXAV.AVnet.Core/Properties/AssemblyInfo.cs
+++ UXAV.AVnet.Core/Properties/AssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9B3BAD80-9F92-4687-915E-C02F3910DCEF")]
 
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
