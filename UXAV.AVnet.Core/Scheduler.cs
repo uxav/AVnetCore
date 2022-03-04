9295b69155dcb95a000c875420ff5ba7d329e129
Mike Jobson <mike@ux.digital>
2022-03-04 13:38:30 +0000

    Update Scheduler.cs
    Added user functions and an edit method. Schedules now return ID for editing.

--- UXAV.AVnet.Core/Scheduler.cs
+++ UXAV.AVnet.Core/Scheduler.cs
@@ -39,9 +39,20 @@ namespace UXAV.AVnet.Core
                     Thread.Sleep(ms);
                 }
             });
+
+            Logger.AddCommand((argString, args, connection, respond) =>
+            {
+                lock (Schedules)
+                {
+                    foreach (var valuePair in Schedules)
+                    {
+                        respond($"{valuePair.Value.Time} -- {valuePair.Value.Callback.Method}\r\n");
+                    }
+                }
+            }, "SchedulesList", "List schedules in scheduler");
         }
 
-        public static void AddSchedule(string time, Action callback)
+        public static int AddSchedule(string time, Action callback)
         {
             if (!Regex.IsMatch(time, @"\d{2}\:\d{2}"))
             {
@@ -67,6 +78,35 @@ namespace UXAV.AVnet.Core
             {
                 _nextId++;
                 Schedules.Add(_nextId, new ScheduleItem(_nextId, time, callback));
+                return _nextId;
+            }
+        }
+
+        public static void EditSchedule(int scheduleId, string time)
+        {
+            if (!Schedules.ContainsKey(scheduleId))
+            {
+                throw new KeyNotFoundException($"No schedule with ID {scheduleId}");
+            }
+
+            if (!Regex.IsMatch(time, @"\d{2}\:\d{2}"))
+            {
+                throw new ArgumentException("Time should be specified as HH:mm", nameof(time));
+            }
+
+            try
+            {
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                DateTime.Parse(time);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Time could not be parsed, {e.Message}", nameof(time));
+            }
+
+            lock (Schedules)
+            {
+                Schedules[scheduleId].Time = time;
             }
         }
 
@@ -77,27 +117,34 @@ namespace UXAV.AVnet.Core
             {
                 foreach (var item in Schedules.Values.Where(i => i.Time == timeString))
                 {
-                    Task.Run(item.Callback);
+                    try
+                    {
+                        Task.Run(item.Callback);
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.Error(e);
+                    }
                 }
             }
         }
     }
 
-    public class ScheduleItem
+    internal class ScheduleItem
     {
         private readonly int _id;
-        private readonly string _time;
         private readonly Action _callback;
 
         internal ScheduleItem(int id, string time, Action callback)
         {
             _id = id;
-            _time = time;
+            Time = time;
             _callback = callback;
         }
 
         public int Id => _id;
-        public string Time => _time;
+        public string Time { get; set; }
+
         public Action Callback => _callback;
     }
 }
