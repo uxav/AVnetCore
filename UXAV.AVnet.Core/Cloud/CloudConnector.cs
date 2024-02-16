f93a272e4d305a7b6818553f2ba9d637829a6389
Mike Jobson <mike@ux.digital>
2024-02-16 04:06:41 +0000

    Changed some logging outputs

--- UXAV.AVnet.Core/Cloud/CloudConnector.cs
+++ UXAV.AVnet.Core/Cloud/CloudConnector.cs
@@ -346,7 +346,7 @@ namespace UXAV.AVnet.Core.Cloud
                 catch (Exception e)
                 {
                     if (_suppressWarning) return;
-                    Logger.Warn($"Could not checkin at {CheckinUri}, {e.Message}");
+                    Logger.Debug($"Could not checkin at {CheckinUri}, {e.Message}");
                     _suppressWarning = true;
                 }
             }
